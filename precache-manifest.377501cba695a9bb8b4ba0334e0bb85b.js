dfd1fd948d3c8169749eb770f3a171a3773995d7
Your Name <matthewabebe.inbox@gmail.com>
2021-04-22 18:20:20 -0400

    Updates

---
+++ precache-manifest.377501cba695a9bb8b4ba0334e0bb85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b506546b95d6fd07861ecc9c3e94018e",
+    "url": "/Leadox/index.html"
+  },
+  {
+    "revision": "cd881357e6c3446c4f9f",
+    "url": "/Leadox/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "2295f6b2a800128de7a6",
+    "url": "/Leadox/static/css/main.14613f72.chunk.css"
+  },
+  {
+    "revision": "cd881357e6c3446c4f9f",
+    "url": "/Leadox/static/js/2.b560884b.chunk.js"
+  },
+  {
+    "revision": "2295f6b2a800128de7a6",
+    "url": "/Leadox/static/js/main.091e1d92.chunk.js"
+  },
+  {
+    "revision": "e2d3b12f1afed793cc01",
+    "url": "/Leadox/static/js/runtime~main.12724981.js"
+  }
+]);
